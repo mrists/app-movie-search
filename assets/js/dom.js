5e4f616469f8872586e86d0f891a46c04e953eb7
mrists <kalashnik.offwh@gmail.com>
2021-10-31 23:05:52 +0300

    app-movie-search done

---
+++ assets/js/dom.js
@@ -0,0 +1,172 @@
+export let movieList = null;
+export let inputSearch = null;
+export let triggerMode = false;
+
+export const createElement = ({
+  type,
+  attrs,
+  container = null,
+  position = 'append',
+  evt = null,
+  handler = null
+}) => {
+  const el = document.createElement(type);
+
+  for (let key in attrs) if (key !== 'innerText') el.setAttribute(key, attrs[key]);
+  else el.innerHTML = attrs[key];
+
+  if (container && position === 'append') container.append(el);
+  if (container && position === 'prepend') container.prepend(el);
+  if (evt && handler) el.addEventListener(evt, handler);
+
+  return el;
+};
+
+export const createStyle = () => {
+  createElement({
+    type: 'style',
+    attrs: {
+      innerText: `
+        * {
+        box-sizing: border-box;
+        }
+        body {
+          margin: 0;
+          font-family: Arial, Helvetica, sans-serif;
+        }
+        .container {
+          padding: 20px;
+          max-width: 1280px;
+          margin: 0 auto;
+        }
+        .movies {
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+          gap: 20px;
+        }
+        .movie {
+          display: flex;
+          align-content: center;
+          justify-content: center;
+        }
+        .movie__image {
+          width: 100%;
+          object-fit: cover;
+        }
+        .search {
+          margin-bottom: 30px;
+        }
+        .search__label-input {
+          display: block;
+          margin-bottom: 7px;
+        } 
+        .search__input {
+          display: block;
+          padding: 10px 15px;
+          width: 400px;
+          border: 1px solid gray;
+          border-radius: 4px;
+          margin-bottom: 10px;
+        }
+        .search__label-checkbox {
+          display: block;
+          font-size: 12px;
+          margin-top: -17px;
+          margin-left: 25px;
+        }`
+    },
+    container: document.head
+  });
+};
+
+export const createMarkup = () => {
+  const container = createElement({
+    type: 'div',
+    attrs: {class: 'container'},
+    container: document.body,
+    position: 'prepend'
+  });
+
+  createElement({
+    type: 'h1',
+    attrs: {innerText: 'Приложение для поиска фильмов'},
+    container
+  });
+
+  const searchBox = createElement({
+    type: 'div',
+    attrs: {class: 'search'},
+    container
+  })
+
+  createElement ({
+    type: 'label',
+    attrs: {
+      class: 'search__label-input',
+      for: 'search',
+      innerText: 'Поиск фильмов'
+    },
+    container: searchBox
+  });
+
+  inputSearch = createElement({
+    type: 'input',
+    attrs: {
+      class: 'search__input',
+      type: 'text',
+      placeholder: 'Начните вводить текст',
+      id: 'search'
+    },
+    container: searchBox
+  });
+
+  createElement({
+    type: 'input',
+    attrs: {
+      class: 'search__checkbox',
+      id: 'checkbox',
+      type: 'checkbox'
+    },
+    container: searchBox,
+    evt: 'click',
+    handler: () => triggerMode = !triggerMode
+  });
+
+  createElement({
+    type: 'label',
+    attrs: {
+      class: 'search__label-checkbox',
+      for: 'checkbox',
+      innerText: 'Добавлять фильмы к существующему списку'
+    },
+    container: searchBox
+  });
+
+  movieList = createElement({
+    type: 'div',
+    attrs: {class: 'movies'},
+    container
+  });
+};
+
+export const addMovieToList = (m) => {
+  const item = createElement({
+    type: 'div',
+    attrs: {class: 'movie'},
+    container: movieList
+  });
+ 
+  createElement({
+    type: 'img',
+    attrs: {
+      class: 'movie__image',
+      src: /^(http|https):\/\//i.test(m.Poster)? m.Poster : 'assets/img/no-image.jpeg',
+      alt: m.Title,
+      title: m.Title
+    },
+    container: item
+  });
+};
+
+export const clearMovieMarkup = () => movieList && (movieList.innerHTML = '');
+
---
+++ assets/js/movies.js
@@ -0,0 +1,57 @@
+import {
+  addMovieToList,
+  clearMovieMarkup,
+  createElement,
+  createMarkup,
+  createStyle,
+  movieList,
+  inputSearch,
+  triggerMode
+} from './dom.js';
+
+let siteUrl = null;
+let searchLast = null;
+
+const debounce = (() => {
+  let timer = null;
+
+  return (cb, ms) => {
+    if (timer !== null) clearTimeout(timer);
+    timer = setTimeout(cb, ms);
+  };
+})();
+
+const getData = (url) => fetch(url)
+  .then((res) => res.json())
+  .then((json) => {
+    if (!json || !json.Search) throw Error('Сервер вернул неправильный объект');
+
+    return json.Search;
+  });
+
+const inputSearchHandler = (e) => {
+  debounce(() => {
+
+    const searchString = e.target.value.trim();
+
+    if (searchString && searchString.length > 3 && searchString !== searchLast) {
+      if (!triggerMode) clearMovieMarkup();
+
+      getData(`${siteUrl}?s=${searchString}&apikey=2645125e`)
+        .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
+        .catch((err) => console.log(err));
+        
+    } 
+
+    searchLast = searchString;
+
+  }, 2000);
+};
+
+export const appInit = () => {
+  createStyle();
+  createMarkup();
+  siteUrl = 'https://www.omdbapi.com/';
+
+  inputSearch.addEventListener('keyup', inputSearchHandler);
+};
